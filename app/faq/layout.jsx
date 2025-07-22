853782388b1d95ca55bf504fda1e6862644f6d60
Thais Mignoli <thais@habii.tech>
2025-07-22 12:45:14 -0300

    fix: faq bg

--- app/faq/layout.jsx
+++ app/faq/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function FaqLayout({ children }) {
   return (
-    <div className="flex flex-col bg-coal-800 flex-1 min-h-screen w-screen overflow-x-hidden bg-[url('@/public/images/faq_bg.png')] bg-center bg-cover">
+    <div className="flex flex-col bg-coal-800 flex-1 min-h-screen w-screen overflow-x-hidden bg-[url('/images/faq_bg.png')] bg-center bg-cover">
       <FaqTopBanner />
       <main className="flex flex-col flex-1 w-full 2xl:max-w-[1920px] mx-auto">
         {children}
