84314d52a9f8053411a902ddc88aae26ed965421
sociedade-socinpro <113698198+sociedade-socinpro@users.noreply.github.com>
2025-08-20 11:21:09 -0300

    Create server.js
    Adiciona server.js para execução com PM2

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const { createServer } = require("http");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
